527494c5ecbb9b7ab5bc7db3486032128f68b106
이서현 <s2018w35@e-mirim.hs.kr>
2022-06-27 09:40:04 +0900

    add expandable layout

--- app/src/main/java/com/seohyunni/cyphersscanner/view/NoticeActivity.kt
+++ app/src/main/java/com/seohyunni/cyphersscanner/view/NoticeActivity.kt
@@ -2,10 +2,33 @@ package com.seohyunni.cyphersscanner.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.seohyunni.cyphersscanner.R
+import com.seohyunni.cyphersscanner.databinding.ActivityNoticeBinding
 
 class NoticeActivity: AppCompatActivity()  {
+
+    private lateinit var dataBinding: ActivityNoticeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_notice)
+
+        dataBinding.expandable.secondLayout.setOnClickListener {
+            dataBinding.expandable.collapse()
+        }
+
+        dataBinding.expandable.parentLayout.setOnClickListener {
+            if(dataBinding.expandable.isExpanded){
+                dataBinding.expandable.collapse()
+            } else{
+                dataBinding.expandable.expand()
+            }
+        }
+
+        dataBinding.backButton.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/seohyunni/cyphersscanner/view/QuestionActivity.kt
+++ app/src/main/java/com/seohyunni/cyphersscanner/view/QuestionActivity.kt
@@ -1,11 +1,31 @@
 package com.seohyunni.cyphersscanner.view
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.seohyunni.cyphersscanner.R
+import com.seohyunni.cyphersscanner.databinding.ActivityQuestionBinding
+
 
 class QuestionActivity: AppCompatActivity()  {
+    private lateinit var dataBinding: ActivityQuestionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_question)
+
+        dataBinding.backButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        when (keyCode){
+            KeyEvent.KEYCODE_BACK -> finish()
+            else -> return super.onKeyDown(keyCode, event)
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
